3d79e537939ec5f8cf01bb645db82970719780e2
ahmedreda286 <ahmedreda2862001@gmail.com>
2023-04-17 16:25:51 +0200

    views2

--- resources/views/machines/List.blade.php
+++ resources/views/machines/List.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{Asset('bootstrap.css.map')}}">
-
+    <link rel="stylesheet" href="{{Asset('bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{Asset('tableStyle.css')}}">
     <title>Machines</title>
 </head>
